04cd6bd463b24be663a40e665a4e0b0eda9aa563
Stefan Savev <st.savev@gmail.com>
2016-03-06 08:52:14 +0100

    format SignatureSpec

--- src/test/scala/com/stefansavev/SignaturesSpec.scala
+++ src/test/scala/com/stefansavev/SignaturesSpec.scala
@@ -1,30 +1,30 @@
 package com.stefansavev
 
-import java.nio.ByteBuffer
 import java.util.Random
-
 import com.stefansavev.randomprojections.datarepr.dense.DataFrameView
 import com.stefansavev.randomprojections.utils.RandomUtils
-import org.scalatest.{Matchers, FlatSpec}
-
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FlatSpec, Matchers}
 import scala.collection.mutable.ArrayBuffer
 
+@RunWith(classOf[JUnitRunner])
 class SignaturesSpec extends FlatSpec with Matchers {
 
   def computeSignaturesFromScratch(rnd: Random, dataFrameView: DataFrameView, numSig: Int): Array[Array[Double]] = {
-    val repeat = numSig*64
+    val repeat = numSig * 64
     val vecs = ArrayBuffer[Array[Double]]()
 
-    for(j <- 0 until repeat){
+    for (j <- 0 until repeat) {
       val v = RandomUtils.generateRandomVector(rnd, dataFrameView.numCols, Array.range(0, dataFrameView.numCols))
       vecs += v.values
     }
 
     val dataFrameViewOutput = ArrayBuffer[Array[Double]]()
-    for(i <- 0 until dataFrameView.numRows){
+    for (i <- 0 until dataFrameView.numRows) {
       val row = dataFrameView.getPointAsDenseVector(i)
       val output = ArrayBuffer[Double]()
-      for(j <- 0 until repeat){
+      for (j <- 0 until repeat) {
         val dp = dotProduct(row, vecs(j))
         val roundedDp = if (dp >= 0.0) 1.0 else -1.0
         output += roundedDp
@@ -39,12 +39,12 @@ class SignaturesSpec extends FlatSpec with Matchers {
     var num1 = 0.0
     var num2 = 0.0
     var i = 0
-    while(i < v1.length){
+    while (i < v1.length) {
       val a = v1(i)
       val b = v2(i)
-      sum += a*b
-      num1 += a*a
-      num2 += b*b
+      sum += a * b
+      num1 += a * a
+      num2 += b * b
       i += 1
     }
     sum
@@ -53,7 +53,7 @@ class SignaturesSpec extends FlatSpec with Matchers {
   def overlapVectors(v1: Array[Double], v2: Array[Double]): Double = {
     var sum = 0.0
     var i = 0
-    while(i < v1.length){
+    while (i < v1.length) {
       val a = v1(i)
       val b = v2(i)
       val s = if (a == b) 1.0 else 0.0
@@ -65,9 +65,9 @@ class SignaturesSpec extends FlatSpec with Matchers {
   }
 
   def cosineFromOverlap(overlap: Double, numSig: Int): Double = {
-    val numSigBits = 64.0*numSig.toDouble //number of bits in signature
+    val numSigBits = 64.0 * numSig.toDouble //number of bits in signature
     val p = overlap.toDouble / numSigBits
-    val angle = (1.0 - p)*Math.PI
+    val angle = (1.0 - p) * Math.PI
     Math.cos(angle)
   }
 
@@ -76,29 +76,29 @@ class SignaturesSpec extends FlatSpec with Matchers {
     var num1 = 0.0
     var num2 = 0.0
     var i = 0
-    while(i < v1.length){
+    while (i < v1.length) {
       val a = v1(i)
       val b = v2(i)
-      sum += a*b
-      num1 += a*a
-      num2 += b*b
+      sum += a * b
+      num1 += a * a
+      num2 += b * b
       i += 1
     }
     num1 = Math.sqrt(num1)
     num2 = Math.sqrt(num2)
     val num = num1 * num2
-    if (num < 1e-5){
+    if (num < 1e-5) {
       0.0
     }
-    else{
-      sum/num
+    else {
+      sum / num
     }
   }
 
   val dataGenSettings = RandomBitStrings.RandomBitSettings(
     numGroups = 1000,
     numRowsPerGroup = 2,
-    numCols=256,
+    numCols = 256,
     per1sInPrototype = 0.5,
     perNoise = 0.2)
 
@@ -112,7 +112,7 @@ class SignaturesSpec extends FlatSpec with Matchers {
   def generateTestFile(): Unit = {
     val fileName = "D:/tmp/cos-overlap.txt"
     val buff = ArrayBuffer[String]()
-    for(i <- 0 until dataGenSettings.numGroups) {
+    for (i <- 0 until dataGenSettings.numGroups) {
       val (p1, p2) = (2 * i, 2 * i + 1)
       val q1 = randomBitStringsDataset.getPointAsDenseVector(p1)
       val q2 = randomBitStringsDataset.getPointAsDenseVector(p2)
@@ -135,7 +135,7 @@ class SignaturesSpec extends FlatSpec with Matchers {
   def estimateAvgError(): (Double, Double) = {
     var errorSum = 0.0
     var errorSumFromScratch = 0.0
-    for(i <- 0 until dataGenSettings.numGroups) {
+    for (i <- 0 until dataGenSettings.numGroups) {
       val (p1, p2) = (2 * i, 2 * i + 1)
       val q1 = randomBitStringsDataset.getPointAsDenseVector(p1)
       val q2 = randomBitStringsDataset.getPointAsDenseVector(p2)
@@ -151,15 +151,15 @@ class SignaturesSpec extends FlatSpec with Matchers {
       errorSum += err
       errorSumFromScratch += Math.abs(cosValue - estCosineFromScratch)
     }
-    val avgError = errorSum/dataGenSettings.numGroups.toDouble
-    val avgErrorFromScratch = errorSumFromScratch/dataGenSettings.numGroups.toDouble
+    val avgError = errorSum / dataGenSettings.numGroups.toDouble
+    val avgErrorFromScratch = errorSumFromScratch / dataGenSettings.numGroups.toDouble
     (avgError, avgErrorFromScratch)
   }
 
   "Error between truth and estimation" should "be small" in {
     val (err, errFromScratch) = estimateAvgError()
     println(s"error between cosine and signature estimation (also error from scratch implementation): ${err} ${errFromScratch}")
-    (err < 0.04) should be (true)
-    (errFromScratch < 0.04) should be (true)
+    (err < 0.04) should be(true)
+    (errFromScratch < 0.04) should be(true)
   }
 }
